985ea6017b2f505130930683966f5e887d776889
Someswar Bhowmick <159946817+someswar07@users.noreply.github.com>
2024-12-28 15:37:01 +0530

    updated code

--- src/main/java/com/practice/ArrayProd.java
+++ src/main/java/com/practice/ArrayProd.java
@@ -1,21 +1,32 @@
 package com.practice;
 
+import java.util.Arrays;
+
 public class ArrayProd {
 
     public int[] productExceptSelf(int[] nums) {
 
         int[] prodArray = new int[nums.length];
         int product = 1;
-        for(int i=0; i< nums.length; i++) {
-            product = product * nums[i];
+        prodArray[0] = nums[0];
+        for(int i=1; i< nums.length; i++) {
+            prodArray[i] = prodArray[i-1] * nums[i];
         }
 
+        for(int i = prodArray.length-1; i >= 0; i--) {
+            if(i==0) {
+                prodArray[i] = product;
+            } else {
+                prodArray[i] = prodArray[i-1] * product;
+                product = product * nums[i];
+            }
+        }
         return prodArray;
     }
 
     public static void main(String[] args) {
         ArrayProd arrayProd = new ArrayProd();
         int nums[] = {1,2,3,4};
-        arrayProd.productExceptSelf(nums);
+        System.out.println(Arrays.toString(Arrays.stream(arrayProd.productExceptSelf(nums)).toArray()));
     }
 }
